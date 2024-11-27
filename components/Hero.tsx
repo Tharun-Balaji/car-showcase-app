d47aff50df58d61ef554632ab950aaefb94a3753
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-27 17:47:04 +0530

    fixed styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@
  * The component is a "client" component, meaning it is rendered on the client-side.
  * The component is exported as the default export from the module.
  */
-'use client'
+"use client";
 
 /**
  * Import the Image component from Next.js, which is used to display the background image.
@@ -20,27 +20,25 @@ import CustomButton from "./CustomButton";
  * The Hero component function.
  */
 function Hero() {
-
   /**
    * The handleScroll function is called when the call-to-action button is clicked.
    * The function is currently empty, but it could be used to scroll to a certain section of the page.
    */
-  const handleScroll = () => { };
+  const handleScroll = () => {};
 
   /**
    * The JSX returned by the component.
    */
   return (
-    <div className="hero" >
-
-      <div className="flex-1 pt-36 padding-x" >
-
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Find, book, or rent a car - quickly and easily!
         </h1>
 
-        <p className="hero__subtitle" >
-          Streamline your car rental experience with our effortless booking process.
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
         </p>
 
         <CustomButton
@@ -48,23 +46,17 @@ function Hero() {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
-
       </div>
 
-      <div className="hero__image-container" >
-
-        <div className="hero__image" >
-
+      <div className="hero__image-container">
+        <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-
-          <div className="hero__image-overlay" />
-
         </div>
 
+        <div className="hero__image-overlay" />
       </div>
-
     </div>
-  )
+  );
 }
 
 /**
