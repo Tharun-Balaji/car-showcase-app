6d7e48049fa6d33c9c159e84d60e844d90867d00
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-27 17:05:22 +0530

    created customButton component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+/**
+ * CustomButton is a reusable component that renders a styled button element.
+ * 
+ * Props:
+ * - title: The text to be displayed inside the button.
+ * - containerStyles: Optional additional CSS classes to apply to the button for styling.
+ * - handleClick: Optional event handler function that is called when the button is clicked.
+ */
+function CustomButton({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+}
+
+export default CustomButton;
