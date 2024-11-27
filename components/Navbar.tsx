948403592b4bdd0cf76332cf2b6a6218d3534d03
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-27 17:33:44 +0530

    added explanatory comments

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,18 @@ import Link from "next/link";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 
+/**
+ * Navbar component renders the navigation bar of the website.
+ * It includes a logo that links to the homepage and a sign-in button.
+ */
 function Navbar() {
   return (
-    <header className="w-full absolute z-10" >
-      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent" >
-        <Link href='/' className="flex justify-center items-center" >
+    // Header element that contains the navigation
+    <header className="w-full absolute z-10">
+      {/* Navigation bar with flex layout and styling */}
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+        {/* Logo linking to the homepage */}
+        <Link href="/" className="flex justify-center items-center">
           <Image
             src="/logo.svg"
             alt="logo"
@@ -16,15 +23,15 @@ function Navbar() {
           />
         </Link>
 
+        {/* Sign-in button using CustomButton component */}
         <CustomButton
           title="Sign In"
           btnType="button"
           containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
         />
-
       </nav>
     </header>
-  )
+  );
 }
 
 export default Navbar;
