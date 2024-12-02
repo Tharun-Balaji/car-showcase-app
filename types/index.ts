39ab13ea4832e3b5b392a9b2f0e8f69f26ea8a88
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-02 19:51:34 +0530

    added textStyles, rightIcon, isDisabled felids to CustomButtonProps type

--- types/index.ts
+++ types/index.ts
@@ -13,13 +13,22 @@ import { MouseEventHandler } from "react";
  * @property {"button" | "submit"} [btnType] - Optional value that changes the
  * button type from the default "button" to "submit". This is useful for
  * submitting forms.
+ * @property {string} [textStyles] - Optional additional CSS classes to
+ * apply to the button text for styling.
+ * @property {string} [rightIcon] - Optional string of SVG code of an icon to
+ * be rendered on the right side of the button.
+ * @property {boolean} [isDisabled] - Optional boolean that disables the button.
  */
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
-  handleClick?: MouseEventHandler<HTMLButtonElement>
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
+
 
 /**
  * @description
@@ -34,4 +43,56 @@ export interface CustomButtonProps {
 export interface SearchManufacturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  /**
+   * The number of miles per gallon that the car can get in city driving.
+   */
+  city_mpg: number;
+  /**
+   * The class of the car. For example, "Compact" or "SUV".
+   */
+  class: string;
+  /**
+   * The number of miles per gallon that the car can get in combination city and
+   * highway driving.
+   */
+  combination_mpg: number;
+  /**
+   * The number of cylinders in the car's engine.
+   */
+  cylinders: number;
+  /**
+   * The displacement of the car's engine.
+   */
+  displacement: number;
+  /**
+   * The type of drive the car has. For example, "Front-Wheel Drive" or "All-Wheel Drive".
+   */
+  drive: string;
+  /**
+   * The type of fuel the car uses. For example, "Gasoline" or "Electric".
+   */
+  fuel_type: string;
+  /**
+   * The number of miles per gallon that the car can get in highway driving.
+   */
+  highway_mpg: number;
+  /**
+   * The make of the car. For example, "Toyota" or "Ford".
+   */
+  make: string;
+  /**
+   * The model of the car. For example, "Corolla" or "F-150".
+   */
+  model: string;
+  /**
+   * The type of transmission the car has. For example, "Automatic" or "Manual".
+   */
+  transmission: string;
+  /**
+   * The year the car was manufactured.
+   */
+  year: number;
 }
