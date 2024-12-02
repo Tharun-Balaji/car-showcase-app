efc1fda932f572ca1e21cdb69a72abee05c1e889
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-02 20:36:33 +0530

    created SearchButton component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,29 @@
 'use client';
 
 import { useState } from "react";
-import {SearchManufacturer} from "./"
+import { SearchManufacturer } from "./";
+import Image from "next/image";
+
+
+/**
+ * The SearchButton component is a button that is used to submit the search form
+ * in the SearchBar component. The button contains a magnifying glass icon.
+ *
+ * @param {{ otherClasses: string }} props
+ * @prop {string} otherClasses - Additional CSS classes to apply to the button
+ * @returns {JSX.Element} A button element with the magnifying glass icon
+ */
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 
 function SearchBar() {
